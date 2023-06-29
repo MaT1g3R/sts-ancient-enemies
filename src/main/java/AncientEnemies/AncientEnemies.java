74fd4ecbdc229e1fe9310b19391dc8de9f789b43
Peijun Ma <peijun.ma@protonmail.com>
2023-06-29 17:55:55 -0400

    Add power string json

--- src/main/java/AncientEnemies/AncientEnemies.java
+++ src/main/java/AncientEnemies/AncientEnemies.java
@@ -1,10 +1,22 @@
 package AncientEnemies;
 
+import basemod.BaseMod;
+import basemod.interfaces.EditStringsSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.localization.PowerStrings;
 
 @SpireInitializer
-public class AncientEnemies {
+public class AncientEnemies implements EditStringsSubscriber {
     public AncientEnemies() {
+        BaseMod.subscribe(this);
+    }
+
+    @Override
+    public void receiveEditStrings() {
+        BaseMod.loadCustomStringsFile(
+                PowerStrings.class,
+                "AncientEnemies/localization/powers.json"
+        );
     }
 
     public static void initialize() {
